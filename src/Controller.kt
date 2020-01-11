fd07cecdfadc8b077942907536927e7c0c98b766
arccassin <arccassin@gmail.com>
2020-01-11 23:12:26 +0300

    delete status, accountWaitedValue, phoneWaitedValue

--- src/Controller.kt
+++ src/Controller.kt
@@ -4,10 +4,7 @@
 
 internal class Controller {
     val commandList = "LIST"
-    private var status: InputStatus = InputStatus.WAIT_NEW_INPUT
     private val phoneBook: PhoneBook
-    private var accountWaitedValue: String? = null
-    private var phoneWaitedValue: String? = null
 
     constructor(phoneBook: PhoneBook) {
         this.phoneBook = phoneBook
@@ -17,60 +14,46 @@ internal class Controller {
         if (input == null){
             return
         }
-        when(status){
-            InputStatus.WAIT_NEW_INPUT -> {
-                if (input.equals(commandList)){
-                        println(phoneBook.getList())
-                } else {
-                    val phoneNumber = FormatPhoneNumber.formatPhone(input)
-                    //если это телефон
-                    if (phoneNumber != null){
-                        val account = phoneBook.getAccountKeyPhone(phoneNumber)
-                        if (account == null){
-                            status = InputStatus.WAIT_ACCOUNT
-                            phoneWaitedValue = phoneNumber
-                            println("Новый телефон, введите имя контакта")
-                        } else {
-                            println("Запись с таким телефоном уже есть:")
-                            println(phoneBook.phoneBookRecordToString(account, phoneNumber))
-                        }
+
+        if (input.equals(commandList)){
+            println(phoneBook.getList())
+            return
+        }
+        val phone = FormatPhoneNumber.formatPhone(input)
+        //this is phone
+        if (phone != null){
+            var account = phoneBook.getAccountKeyPhone(phone)
+            if (account == null){
+                println("Новый телефон, введите имя контакта")
+                account = readLine()
+                phoneBook.addPhoneBookRecord(account, phone);
+                println(phoneBook.phoneBookRecordToString(account, phone))
+                println("Новая запись добавлена")
+            } else {
+                println("Запись с таким телефоном уже есть:")
+                println(phoneBook.phoneBookRecordToString(account, phone))
+            }
+        } else {
+            val account = input
+            var phone = phoneBook.getPhoneKeyAccount(account)
+            if (phone == null){
+                println("Новое имя, введите телефон")
+                while (true) {
+                    val inline = readLine()
+                    phone = FormatPhoneNumber.formatPhone(inline)
+                    if (phone == null) {
+                        println("\"$inline\" - Неверный формат номера телефона, попробуйте еще раз")
+                        continue
                     } else {
-                        val account = input
-                        val phone = phoneBook.getPhoneKeyAccount(account);
-                        if (phone == null){
-                            status = InputStatus.WAIT_PHONE
-                            accountWaitedValue = account
-                            println("Новое имя, введите телефон")
-                        } else{
-                            println("Запись с таким именем уже есть:")
-                            println(phoneBook.phoneBookRecordToString(account, phone))
-                        }
+                        break;
                     }
                 }
-            }
-            InputStatus.WAIT_ACCOUNT -> {
-                val account = input
-                phoneBook.addPhoneBookRecord(account, phoneWaitedValue);
-                println(phoneBook.phoneBookRecordToString(account, phoneWaitedValue))
+                println(phoneBook.phoneBookRecordToString(account, phone))
                 println("Новая запись добавлена")
-                status = InputStatus.WAIT_NEW_INPUT
-            }
-            InputStatus.WAIT_PHONE -> {
-                val phone = FormatPhoneNumber.formatPhone(input)
-                if (phone == null){
-                    println("\"$input\" - Неверный формат номера телефона")
-                    status = InputStatus.WAIT_NEW_INPUT
-                    return
-                }
-                phoneBook.addPhoneBookRecord(accountWaitedValue, phone);
-                println(phoneBook.phoneBookRecordToString(accountWaitedValue, phone))
-                println("Новая запись добавлена")
-                status = InputStatus.WAIT_NEW_INPUT
+            } else {
+                println("Запись с таким именем уже есть:")
+                println(phoneBook.phoneBookRecordToString(account, phone))
             }
         }
     }
-}
-
-enum class InputStatus{
-    WAIT_PHONE, WAIT_ACCOUNT, WAIT_NEW_INPUT
 }
